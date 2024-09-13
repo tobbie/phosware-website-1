79c3ea7d151ebd4fe57aeb522cdba83c99faa354
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2024-09-13 17:35:36 +0100

    home page text additions

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
 // This file is intentionally blank
